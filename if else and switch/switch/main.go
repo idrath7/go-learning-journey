d55f0f4c1c7223505279d9ff49fc1c1b6e1c374c
Idrath Hossan Zidan <idrathhossain7@.in>
2026-05-23 12:44:00 +0600

    add if else and switch examples

---
+++ if else and switch/switch/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 3
+
+	switch a {
+	case 1:
+		fmt.Println("a is 1")
+
+	case 2, 3:
+		fmt.Println("a is either 2 or 3")
+
+	default:
+		fmt.Println("None")
+	}
+}
