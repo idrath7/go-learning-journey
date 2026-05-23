a20b4912482737cc57d6d6043627b7dfaea5bade
Idrath Hossan Zidan <idrathhossain7@.in>
2026-05-23 13:36:27 +0600

    add function examples

---
+++ scanln/basic/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to the application")
+	var name string
+	fmt.Println("Enter your name-")
+	fmt.Scanln(&name)
+	fmt.Println("Your name is", name)
+}
