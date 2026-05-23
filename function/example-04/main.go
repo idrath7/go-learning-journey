525dea7d05a7997cb73433c438339bcbb6b737e1
Idrath Hossan Zidan <idrathhossain7@.in>
2026-05-23 13:24:52 +0600

    add function examples

---
+++ function/example-04/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func printsomething() {
+	fmt.Println("Education must be free!")
+}
+func sayhello(name string) {
+	fmt.Println("Welcome,", name)
+}
+func main() {
+	printsomething()
+	sayhello("Idrath")
+}
