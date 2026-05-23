1379582f8c55f058eb4d1b99efb9c7bc45cbeb8e
Idrath Hossan Zidan <idrathhossain7@.in>
2026-05-23 15:01:17 +0600

    Fix logging config and update dependencies

---
+++ scope/package scope/one folder two file/add.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func add(n1 int, n2 int) {
+	rslt := n1 + n2
+	fmt.Println(rslt)
+}
---
+++ scope/package scope/one folder two file/main.go
@@ -0,0 +1,8 @@
+package main
+
+var a = 20
+var b = 30
+
+func main() {
+	add(4, 7)
+}
