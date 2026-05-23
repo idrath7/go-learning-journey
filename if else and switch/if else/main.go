8ed5ac09039e5ae886e010a602967bd611be62b8
Idrath Hossan Zidan <idrathhossain7@.in>
2026-05-24 01:01:44 +0600

    add scope examples

---
+++ if else and switch/if else/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 18
+
+	if age > 18 {
+		fmt.Println("You are eligible to be married")
+	} else if age < 18 {
+		fmt.Println("You are not eligible to be married, but you can love someone")
+	} else if age == 18 {
+		fmt.Println("You are just a teenager, not eligible to be married")
+	}
+}
---
+++ scope/local scope/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var (
+	a = 20
+	b = 30
+)
+
+func main() {
+	x := 18
+
+	if x >= 18 {
+		p := 10
+
+		fmt.Println("I'm matured boy")
+		fmt.Println("I've", p, "girlfriends")
+	}
+}
